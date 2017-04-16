f607f9d3773d9b86b5f5839f75d6da3fd2b00a53
ChiharuTerashima <chichi.pa2.1091@gmail.com>
2017-04-16 22:16:00 +0900

    * .gitignore: 新規作成
    * Main.cs:
    * ChatClient.sln:
    * Info.plist:
    * Tests.cs:
    * AppDelegate.cs:
    * App.xaml:
    * packages.config:
    * MainActivity.cs:
    * packages.config:
    * App.xaml.cs:
    * Entitlements.plist:
    * packages.config:
    * AppInitializer.cs:
    * ChatClient.iOS.csproj:
    * packages.config:
    * LaunchScreen.storyboard:
    * ChatClient.csproj:
    * AboutAssets.txt:
    * ChatClient.Droid.csproj:
    * ChatClientPage.xaml:
    * AssemblyInfo.cs:
    * icon.png:
    * styles.xml:
    * ChatClient.UITests.csproj:
    * ChatClientPage.xaml.cs:
    * Contents.json:
    * Tabbar.axml:
    * AboutResources.txt:
    * Toolbar.axml:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * AssemblyInfo.cs:
    * icon.png:
    * icon.png:
    * icon.png:
    * Contents.json:

---
+++ ChatClient/ChatClientPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WebSocket.Portable;
+using WebSocket.Portable.Interfaces;
+using Websockets;
+using Xamarin.Forms;
+
+namespace ChatClient
+{
+	public partial class ChatClientPage : ContentPage
+	{
+		private IWebSocketConnection client;
+		public ChatClientPage()
+		{
+			InitializeComponent();
+			Connect();
+		}
+
+		private void Connect()
+		{
+			client = Websockets.WebSocketFactory.Create();
+			client.OnLog += Connection_OnLog;
+			client.OnError += Connection_OnError;
+			client.OnMessage += Connection_OnMessage;
+			client.OnOpened += Connection_OnOpened;
+			client.Open("wss://submeeting.herokuapp.com/websocket");
+		}
+
+		private void BtnClieckd(object sender, EventArgs e) {
+			client.Send(this.name.Text + ": " + this.message.Text);
+		}
+
+		private void Connection_OnOpened()
+		{
+			Debug.WriteLine("Connection_OnOpened");
+		}
+
+		private void Connection_OnMessage(String obj)
+		{
+			var label = new Label{ Text = obj.ToString() };
+			this.sl.Children.Add(label);
+		}
+
+		private void Connection_OnError(string obj)
+		{
+			Debug.WriteLine("ERROR " + obj);
+		}
+
+		private void Connection_OnLog(string obj)
+		{
+			Debug.WriteLine(obj);
+		}
+	}
+}
